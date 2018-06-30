3db4fc5e656320717e8e67732a1b042fa8f46409
3211w3211 <40725237+3211w3211@users.noreply.github.com>
2018-07-01 01:10:15 +0300

    Update config.php

--- www/config.php
+++ www/config.php
@@ -6,5 +6,4 @@ define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
 define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
 
 define('CALLBACK_API_CONFIRMATION_TOKEN', 'bb112c41'); //Строка для подтверждения адреса сервера из настроек Callback API
-define('VK_API_ACCESS_TOKEN', '016cb55230eb83e34f620389s149345d975bc12e2fb6f30e0a833a51d345d0d123c3dc0abc1c864036sdf989fb8345'); //Ключ доступа сообщества
-define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit
+define('VK_API_ACCESS_TOKEN', '90d358cccf02bcc8afdf357bcbe9436edbd9249324671643c60968f089ee8e19393c9a6865e7e1ed95a75'); //Ключ доступа сообщества
